a66c9bbf21859fbb1bf1e7f442219d04b050887c
Anant Jain <anantjain98@me.com>
2017-10-28 20:55:47 -0700

    Configure Firebase

--- TUX/App.js
+++ TUX/App.js
@@ -4,10 +4,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "<your-api-key>",
-  authDomain: "<your-auth-domain>",
-  databaseURL: "<your-database-url>",
-  storageBucket: "<your-storage-bucket>",
+  apiKey: "AIzaSyBCHupVzHelOl7mreRJwi2KxaANbh1lGcw",
+  authDomain: "tinderforuberforx.firebaseapp.com",
+  databaseURL: "https://tinderforuberforx.firebaseio.com",
+  projectId: "tinderforuberforx",
+  storageBucket: "",
+  messagingSenderId: "302054829899"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
